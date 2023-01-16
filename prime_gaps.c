4412e32f51217740f433cb54de0ebb83ce4770b4
Zamil Bahri <zamilbahri7@gmail.com>
2023-01-16 17:43:39 -0500

    added README

--- prime_gaps.c
+++ prime_gaps.c
@@ -3,14 +3,6 @@
 #include <math.h>
 #include "mpi.h"
 
-/*
-Issues:
-- If the largest gap exists between the last prime in one thread, and first prime in second thread,
-	then that gap will not be recognized.
-- Efficiency issue: dividing N into equally sized chunks is not efficient, since it takes much longer
-	to for larger numbers to check if they are prime, than smaller numbers.
-*/
-
 #define N 1000
 #define MAX(A, B) (A > B ? A : B)
 #define MIN(A, B) (A < B ? A : B)
