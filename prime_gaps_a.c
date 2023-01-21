62ed735796a0c3b98d7435850caefaa897cb81f0
Zamil Bahri <zamilbahri@gmail.com>
2023-01-20 19:36:11 -0500

    can accomodate unsigned int

--- prime_gaps_a.c
+++ prime_gaps_a.c
@@ -6,43 +6,18 @@
 
 #define MAX(A, B) (A > B ? A : B)
 #define MIN(A, B) (A < B ? A : B)
-#define REPS 25
+#define REPS 50
 
 typedef struct ProcessInfo {
-	int first_prime;
-	int last_prime;
+	unsigned long first_prime;
+	unsigned long last_prime;
 } ProcessInfo;
-
-// int isPrime(int n) {
-// 		if (n <= 1) return 0;
-//     if (n <= 3) return 1;
-// 		if (n % 2 == 0 || n % 3 == 0) return 0;
-    
-//     for (int i=5; i*i <= n; i += 6) {
-//         if (n % i == 0 || n % (i + 2) == 0) return 0;
-//     }
-//     return 1;
-// }
-
-// float f(float n) {
-// 	return (2.0/9)*pow(n, 1.5);
-// }
-
-// void findBounds(int n, int size, int rank, float *start, float *end) {
-// 	float area = f(n)/size;
-	
-// 	*start = 0;
-// 	for (int i = 0; i <= rank; i++) {
-// 		*end = pow((4.5)*(area+f(*start)), 2/3);
-// 		if (i != rank) *start = ceil(*end);
-// 	}
-// 	*start = ceil(*start);
-// }
 
 int main(int argc, char** argv) {
     // Initialize the MPI environment
 		int rank, size;
-		int N = atoi(argv[1]);
+		char *ptr;
+		unsigned long N = strtoul(argv[1], &ptr, 10);
 
 		double time1, time2, duration, global;
 
@@ -55,20 +30,19 @@ int main(int argc, char** argv) {
     // Get the rank of the process
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-		int start = 5 + rank * (N/size);
-		int end = MIN(start + (N/size), N);
-
-		int localcount=0;
+		unsigned long start = 5 + rank * (N/size);
+		unsigned long end = MIN(start + (N/size), N);
 
 		mpz_t mpz_prime;
 		mpz_init(mpz_prime);
 
-		int gap = 0; // running gap between primes
-		int prime; // stores the prime associated with the largest gap
-		int prev = -1; // keeps track of previous prime
+		unsigned long gap = 0; // running gap between primes
+		unsigned long prime; // stores the prime associated with the largest gap
+		unsigned long prev = 0; // keeps track of previous prime
 
-		int local_primegap[2]; // stores largest gap and the prime associated with it.
-		ProcessInfo pi; // stores the first and last primes in this process
+		unsigned long local_primegap[2]; // stores largest gap and the prime associated with it.
+		unsigned long first_last_primes[2]; // stores the first and last primes in this process
+		// ProcessInfo pi; // stores the first and last primes in this process
 
 		// Strategy, we only need to check numbers of the form 6k-1 and 6k+1.
 		//	Why? Because:
@@ -87,7 +61,7 @@ int main(int argc, char** argv) {
 		if (start % 6 == 1) {
 			mpz_set_ui(mpz_prime, (unsigned long) start);
 			if (mpz_probab_prime_p(mpz_prime, REPS) >= 1) {
-				pi.first_prime = start;
+				first_last_primes[0] = start;
 				prime = start;
 				prev = start;
 			}
@@ -96,13 +70,13 @@ int main(int argc, char** argv) {
 
 		// Start with number of the form 6k-1, then check
 		// 6k+1, then increment by 6
-    for (int i = 0; start + i < end; i+=6) {
-				localcount++;
-				mpz_set_ui(mpz_prime, (unsigned long) start+i);
+    for (unsigned long i = 0; start + i < end; i+=6) {
+
+				mpz_set_ui(mpz_prime, start+i);
         if (mpz_probab_prime_p(mpz_prime, REPS) >= 1) {
 					// set "previous" prime to the first prime encountered in this thread
-					if (prev < 0) {
-						pi.first_prime = start + i;
+					if (prev == 0) {
+						first_last_primes[0] = start + i;
 						prev = start + i;
 					}
 
@@ -113,11 +87,11 @@ int main(int argc, char** argv) {
 					prev = start+i;
 				}
 
-				mpz_set_ui(mpz_prime, (unsigned long) start + i+2);
+				mpz_set_ui(mpz_prime, start + i+2);
         if (start + i+2 < end && mpz_probab_prime_p(mpz_prime, REPS) >= 1) {
 					// set "previous" prime to the first prime encountered in this thread
-					if (prev < 0) {
-						pi.first_prime = start + i+2;
+					if (prev == 0) {
+						first_last_primes[0] = start + i+2;
 						prev = start + i+2;
 					}
 
@@ -131,7 +105,7 @@ int main(int argc, char** argv) {
 		time2 = MPI_Wtime();
 		duration = time2-time1;
 
-		pi.last_prime = prev;
+		first_last_primes[1] = prev;
 
 		local_primegap[0] = gap;
 		local_primegap[1] = prime;
@@ -139,15 +113,12 @@ int main(int argc, char** argv) {
 
 		// Send first prime and last prime info to the root processor (0).
 		if (rank != 0) {
-			MPI_Send(&pi, 2, MPI_INT, 0, 1, MPI_COMM_WORLD);
+			MPI_Send(first_last_primes, 2, MPI_UNSIGNED_LONG, 0, 1, MPI_COMM_WORLD);
 		}
 
 
-    int global_primegap[2];
-    MPI_Reduce(local_primegap, global_primegap, 2, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
-
-		int globalcount;
-		MPI_Reduce(&localcount, &globalcount, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    unsigned long global_primegap[2];
+    MPI_Reduce(local_primegap, global_primegap, 2, MPI_UNSIGNED_LONG, MPI_MAX, 0, MPI_COMM_WORLD);
 
 		MPI_Reduce(&duration, &global, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);		
 		
@@ -156,34 +127,33 @@ int main(int argc, char** argv) {
 		if (rank == 0) {
 
 			// initialize an array to store the first and last primes from each processor
-			int first_last_primes[size*2];
+			unsigned long all_first_last_primes[size*2];
 
 			// store first and last primes from root processor
-			first_last_primes[0] = pi.first_prime;
-			first_last_primes[1] = pi.last_prime;
+			all_first_last_primes[0] = first_last_primes[0];
+			all_first_last_primes[1] = first_last_primes[1];
 
 			// get first and last primes from other processors
 			MPI_Status status;
 			for (int i=1; i < size; ++i) {
-				// Can reuse "pi" variable since info has alraedy been stored in array.
-				MPI_Recv(&pi, 2, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
-				first_last_primes[i*2] = pi.first_prime;
-				first_last_primes[i*2+1] = pi.last_prime;
+				// Can reuse first_last_primes since it'sinfo has already been recorded
+				MPI_Recv(first_last_primes, 2, MPI_UNSIGNED_LONG, i, 1, MPI_COMM_WORLD, &status);
+				all_first_last_primes[i*2] = first_last_primes[0];
+				all_first_last_primes[i*2+1] = first_last_primes;
 			}
 
 			// check if previous best prime gap has been beat
 			int diff;
 			for (int i = 2; i < size*2-1; i+=2) {
-				diff = first_last_primes[i] - first_last_primes[i-1];
+				diff = all_first_last_primes[i] - all_first_last_primes[i-1];
 				if (diff > global_primegap[0]) {
 					global_primegap[0] = diff;
-					global_primegap[1] = first_last_primes[i];
+					global_primegap[1] = all_first_last_primes[i];
 				}
 			}
 
-			printf("Largest gap in primes less than %d: %d\n which occured between %d and %d\n", N, global_primegap[0], global_primegap[1]-global_primegap[0], global_primegap[1]);
+			printf("Largest gap in primes less than %lu: %lu\n which occured between %lu and %lu\n", N, global_primegap[0], global_primegap[1]-global_primegap[0], global_primegap[1]);
 			printf("global runtime is %f\n", global);
-			printf("%d loops\n", globalcount);
 		}
 
     // Finalize the MPI environment.
